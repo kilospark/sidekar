181e823c26998b188b9e54b1e90bbe3cd3119146
karthik <karthiknb@gmail.com>
2026-04-06 09:04:56 -0400

    Refactor REPL colors to centralized module for easy theming; add JPEG output, resizing (default 800px width), and quality control to desktop screenshot command

---
+++ src/repl/colors.rs
@@ -0,0 +1,12 @@
+pub mod colors {
+    pub const RESET: &str = "\x1b[0m";
+    pub const BOLD: &str = "\x1b[1m";
+    pub const DIM: &str = "\x1b[2m";
+    pub const BLUE: &str = "\x1b[34m";
+    pub const CYAN: &str = "\x1b[36m";
+    pub const GREEN: &str = "\x1b[32m";
+    pub const YELLOW: &str = "\x1b[33m";
+    pub const RED: &str = "\x1b[31m";
+}
+
+pub use colors::{BLUE, BOLD, CYAN, DIM, GREEN, RED, RESET, YELLOW};
